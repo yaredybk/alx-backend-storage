fca0b7f34812fc6545d7fb2e0e5907e230ae091c
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-08-09 05:25:36 +0300

    syntax trial

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,8 +6,9 @@
 -- name, string (255 characters)
 -- If the table already exists, your script should not fail
 -- Your script can be executed on any database
-CREATE TABLE IF NOT EXISTS `users` (
-	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`email` VARCHAR(255) NOT NULL UNIQUE,
-	`name` VARCHAR(255)
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
 )
