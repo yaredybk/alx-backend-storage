92603e6c3c7939e3dcb28ad312ff39dfe0664560
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-08-13 20:42:09 +0300

    fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,4 +10,4 @@ def update_topics(mongo_collection, name, topics):
   name (string) will be the school name to update
   topics (list of strings) will be the list of topics approached in the school
   """
-  mongo_collection.update_one({"name":name},{"$set":{topics:topics}})
+  mongo_collection.update_many({"name":name},{"$set": {topics:topics}})
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -14,4 +14,4 @@ def schools_by_topic(mongo_collection, topic):
   Returns:
     list: list of school having a specific topic:
   """
-  mongo_collection.find({"topic":topic})
+  return mongo_collection.find({"topic":topic})
