5a07ab2622286d50ce1defa695226587ccced9b7
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-08-13 21:18:48 +0300

    pycodestyle

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,7 +9,8 @@ def nginx_stat:
   """
   first line: x logs where x is the number of documents in this collection
   second line: Methods:
-  5 lines with the number of documents with the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order (see example below - warning: it’s a tabulation before each line)
+  5 lines with the number of documents 
+  with the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order 
   one line with the number of documents with:
   method=GET
   path=/status
