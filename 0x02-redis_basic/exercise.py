8e52824b1e309720e0396351424736fd1d5974ef
yared b. <yb12ybk@gmail.com>
2024-08-17 06:01:16 +0300

    almost done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,14 +9,47 @@ from functool import wraps
 
 
 def count_calls(method: Callable) -> Callable:
+    """
+    Creates and returns function that increments the count
+    for that key every time the method is called and returns
+    the value returned by the original method
+    """
     @wraps(method)
     def incr_wrapper(self,*args, **kwargs):
         counter_name = method.__qualname__
-        count = self._redis.incr(counter_name) or 0
+        self._redis.incr(counter_name)
         return method(self,*args, **kwargs)
     return incr_wrapper
 
-@count_calls
+
+def call_history(method: Callable) -> Callable:
+    """Stores the history of inputs and outputs for a particular function.
+    """
+    method_key = method.__qualname__
+    inputs, outputs = method_key + ':inputs', method_key + ':outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(inputs, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(result))
+        return result
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """Displays the history of calls of a particular function"""
+    method_key = method.__qualname__
+    inputs, outputs = method_key + ':inputs', method_key + ':outputs'
+    redis = method.__self__._redis
+    method_count = redis.get(method_key).decode('utf-8')
+    print(f'{method_key} was called {method_count} times:')
+    IOTuple = zip(redis.lrange(inputs, 0, -1), redis.lrange(outputs, 0, -1))
+    for inp, outp in list(IOTuple):
+        attr, data = inp.decode("utf-8"), outp.decode("utf-8")
+        print(f'{method_key}(*{attr}) -> {data}')
+
+
 class Cache:
   """Basic cache in redis."""
 
@@ -29,7 +62,9 @@ class Cache:
     self._redis = redis.Redis()
     self._redis.flushdb()
 
-  def store(self, data: Union[str, bytes, int, float]) -> str:
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
     """Store the input data in Redis using the random key and return the key."""
     id: str = str(uuid4())
     if not isinstance(data, (str, bytes, int, float)):
